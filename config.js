6c4734df532c02a30fce2ed649263c2cf94949ca
tuan-kiet2211 <85292702+tuan-kiet2211@users.noreply.github.com>
2021-06-03 19:29:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trên thế giới có hơn 7 tỷ nụ cười nhưng mà với anh nụ cười em mới là đẹp nhất
+                Thế nên là làm ơn hãy để anh giúp em giữ nụ cười của em trên môi`
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Hey girl 😙',
+    desc: 'anh đã trằn trọc suốt đêm qua đặt ra một câu hỏi tại sao tên em không phải là MẪN NHI. Em có thắc mắc về điều đó không ♥ '
+    btnYes: 'Whyyyyyyy <33',
+    btnNo: 'Hông quan tâm :3',
+    question:'Bởi vì em chính là MĨ NHÂN của cuộc đời anh <3',
+    btnReply: 'Em àaaa <3',
+    reply: 'rái đất vốn lạ thường 
+            Mà sao em cứ đi nhầm đường
+            Lạc vào tim anh lẻ loi
+            Đằng sau chữ yêu đây là thương 🥰🥰🥰',
+    mess: 'Quỷ hàaaaaaaaaa😘😘',
+    messDesc: 'Cuộc sống anh là của em.',
+    btnAccept: 'Hihihihi <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100025228957958'
 }
