0c64b7ef8f7bc17ecf3708f32f20ec339924102a
tuan-kiet2211 <85292702+tuan-kiet2211@users.noreply.github.com>
2021-06-03 19:58:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: '🥰🥰🥰🥰Vângg <33',
+    btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ nụ cười, nhưng đối với anh nụ cười của em là đẹp nhất <3',
-    btnReply: 'Hic <3',
-    reply: 'Vậy nên làm ơn, để anh giúp em giữ nụ cười của em trên môi <33333333',
-    mess: '🥰🥰 Quỷ xứ hafaaaaaa :333 😘😘',
-    messDesc: 'Anh sẽ luôn là của em.',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://100025228957958' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
